98cbce7585dec8309c96411a3edcf613f8ed547b
Duarte099 <a14395@oficina.pt>
2026-04-14 12:19:07 +0100

    correção de erro

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 let AuthAPI = require("./server/auth");
+let SpacesAPI = require("./server/spaces");
 function initialize() {
-  let api = express();
-  api.use("/auth", AuthAPI());
-  return api;
+    let api = express();
+    api.use("/auth", AuthAPI());
+    api.use("/spaces", SpacesAPI());
+    return api;
 }
 module.exports = {
-  initialize: initialize,
+    initialize: initialize,
 };
